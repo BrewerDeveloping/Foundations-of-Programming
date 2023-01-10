213111010560a54c63fb2660cbe073ccc7b127b1
BrewerDeveloping <115757514+BrewerDeveloping@users.noreply.github.com>
2023-01-09 19:07:28 -0600

    First big save

--- Notes.py
+++ Notes.py
@@ -1,1 +1,7 @@
-print("Hello World")
+###--Git Notes--##
+# $ git init    //Initialze Local Git Repository
+# $ git add <file>    // Add File(s) to Index
+# $ git status      //Check Status of working Tree
+# $ git commit      //Commit Changes to index
+# $ git push        // Push to remote repository
+# $ git pull        // Pull from remote repository
